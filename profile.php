9359d4a1e0bf953ce35200e1355458e90d9117ea
jlwine <jlwine@mail.ru>
2021-09-29 14:23:12 +0700

    стилизовал профиль(с багами) позже исправлю

--- profile.php
+++ profile.php
@@ -12,10 +12,22 @@ session_start();
     <link rel="stylesheet" href="styleProfile.css"
 </head>
 <body>
+    <header class="header">
+        <img class="logo" src="img/logo.png" alt="logo" width="50" height="50">
+        <nav>
+            <ul class="nav__links">
+                <li><a href="#">Обо мне</a></li>
+                <li><a href="#">Обратная связь</a></li>
+            </ul>
+        </nav>
+    </header>
     <div class="container">
-        <img style="border-radius: 50px;" src="<?=$_SESSION['user']['avatar'] ?>" width="200" alt="">
-        <h2 style="margin: 10px 0;"><?= $_SESSION['user']['login'] ?></h2>
-        <a href="#"><?= $_SESSION['user']['email']?></a>
+        <img style="border-radius: 50px;" src="<?=$_SESSION['user']['avatar'] ?>" width="200" height="188" alt="">
+        <br>
+        <h2 style="margin: 10px 0;" class="login"><b><?= $_SESSION['user']['login'] ?></b></h2></h2>
+        <br>
+        <a href="#" class="email">Email: <div class="emailUser"><?= $_SESSION['user']['email']?></div></a><br>
+        <a href="index.php" class="my_tasks">Мои задачи</a><br>
         <a href="auth.php" class="logout">Выйти</a>
     </div>
 </body>
