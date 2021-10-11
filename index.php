c8932c1df5998318525d1efadf32946a7d1d2c97
jlwine <jlwine@mail.ru>
2021-10-11 14:06:19 +0700

    Если пользователь авторизован добавил кнопку выйти и ссылку на профиль

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $id_user = $_GET['id'];
 $_SESSION['id'] = $id_user;
 
 
+   
+
+
 ?>
 <!doctype html>
 <html lang="ru">
@@ -17,6 +20,7 @@ $_SESSION['id'] = $id_user;
 </head>
 <body>
     <header class="header">
+        
         <img class="logo" src="img/logo.png" alt="logo" width="50" height="50">
         <nav>
             <ul class="nav__links">
@@ -24,10 +28,20 @@ $_SESSION['id'] = $id_user;
                 <li><a href="#">Обратная связь</a></li>
             </ul>
         </nav>
-        <div class="btns">
-        <a class="login" href="auth.php"><button class="Auth">Login</button></a>
-        <a class="sign" href="signup.php"><button class="Register">Sign up</button></a>
-        </div>
+        <a class="profile_avatar" href="profile.php"><img style="border-radius: 100px;" src="<?=$_SESSION['user']['avatar'] ?>" width="55" height="55" alt=""></a>
+        <a class="nickname" href="profile.php"><?=$_SESSION['user']['login']?></a>
+        <?php
+        if(($_SESSION['user']['login'] == NULL) && ($_SESSION['user']['email'] == NULL)) {
+            echo '<div class="btns">
+                 <a class="login" href="auth.php"><button class="Auth">Login</button></a>
+                 <a class="sign" href="signup.php"><button class="Register">Sign up</button></a>
+                 </div>';
+         } else {
+             echo '<a class="header" href="logout.php"><button class="Register">Exit</button></a>';
+         }
+        ?>
+        
+        
     </header>
 
 <div class="container">
@@ -49,19 +63,6 @@ $_SESSION['id'] = $id_user;
             echo '<li class="lii"><b>'.$row->task.'</b><a href="/delete.php?id='.$row->id.'"><button>Удалить</button></a></li>';
         }
         echo '</ul>'
-
-        // $connect = mysqli_connect('localhost', 'mysql', '', 'to-do');
-        // $idUser = $_GET['id'];
-        
-        // echo '<ul class="ull">';
-        // $query = $connect -> query('SELECT * FROM `tasks` WHERE `id_user` ='.$idUser);
-        // while($row = $query->fetch(PDO::FETCH_OBJ)) {
-        //     echo '<li class="lii"><b>'.$row->task.'</b><a href="/delete.php?id='.$row->id.'"><button>Удалить</button></a></li>';
-        // }
-        // echo '</ul>'
-
-        
-        
         ?>
 
 </div>
